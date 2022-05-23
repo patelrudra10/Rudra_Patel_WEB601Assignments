99d2d2cc0dee936481ff487b1757b5ef38e0bab7
patelrudra10 <RP119@myscc.ca>
2022-05-23 19:42:00 -0400

    Commit 04

--- R_Patel_StrangerThings/src/app/content-card/content-card.component.ts
+++ R_Patel_StrangerThings/src/app/content-card/content-card.component.ts
@@ -1,14 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import {Content} from '../models/content';
 
 @Component({
   selector: 'app-content-card',
   templateUrl: './content-card.component.html',
-  styleUrls: ['./content-card.component.css']
+  styleUrls: ['./content-card.component.scss']
 })
+
 export class ContentCardComponent implements OnInit {
+  @Input() char?: Content;
+  static characterCount = 0;
+  private characters: Content[];
 
-  constructor() { }
-
+  constructor() {
+    this.characters = []; 
+  }
+  get character():Content[] {
+    return this.characters;
+  }
+  mychardetails(){
+    console.log(this.char?.id);
+    console.log(this.char?.author);
+  }
   ngOnInit(): void {
   }
 
