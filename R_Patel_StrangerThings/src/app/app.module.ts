1c0aad2aad57b3a3d8030fbaca85c11d257048e9
patelrudra10 <RP119@myscc.ca>
2022-05-31 14:35:35 -0400

    Commit 02

--- R_Patel_StrangerThings/src/app/app.module.ts
+++ R_Patel_StrangerThings/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ContentListComponent } from './content-list/content-list.component';
 import { ContentCardComponent } from './content-card/content-card.component';
+import { TypePipe } from './type.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
     ContentListComponent,
-    ContentCardComponent
+    ContentCardComponent,
+    TypePipe
   ],
   imports: [
     BrowserModule,
---
+++ R_Patel_StrangerThings/src/app/type.pipe.ts
@@ -0,0 +1,16 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import { Content } from './models/content';
+
+@Pipe({
+  name: 'type'
+})
+export class TypePipe implements PipeTransform {
+
+  transform(strangerthings:Content[], typeInput?: string): Content[] {
+    return  strangerthings.filter(character =>{
+      return !character.type?.length || character.type =="Superhuman" || character.type =="Human";
+
+    })
+  }
+
+}
