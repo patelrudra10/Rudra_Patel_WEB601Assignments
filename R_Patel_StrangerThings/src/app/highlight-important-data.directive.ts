07ee119f5e973d6da8dd160051773f2eaeac6f09
patelrudra10 <RP119@myscc.ca>
2022-06-07 15:57:24 -0400

    Border-Type Done

--- R_Patel_StrangerThings/src/app/highlight-important-data.directive.ts
+++ R_Patel_StrangerThings/src/app/highlight-important-data.directive.ts
@@ -1,10 +1,26 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef, HostBinding, HostListener, Input } from '@angular/core';
 
 @Directive({
   selector: '[appHighlightImportantData]'
 })
 export class HighlightImportantDataDirective {
+  @Input() color?: string;
+  private isHighlighted: boolean = false;
+  private initialColour: string;
 
-  constructor() { }
+  @HostBinding('style.border')
+  get border() {
+    return this.isHighlighted ? '3px solid black' : this.initialColour;
+  }
+  constructor(private el: ElementRef) {
+    this.initialColour = this.el.nativeElement.style.border;
+  }
+
+  @HostListener('mousein') onclick() {
+    this.isHighlighted = !this.isHighlighted;
+  }
+  @HostListener('mouseout') onClick() {
+    this.isHighlighted = !this.isHighlighted;
+  }
 
 }
